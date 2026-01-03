51e46786d7ad0b6ce806af6d86fef638dfe2d305
ichiil <bloodgamer776@gmail.com>
2026-01-03 22:35:33 +0700

    Реагирование на клики

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -14,6 +14,7 @@ import androidx.glance.GlanceTheme
 import androidx.glance.Image
 import androidx.glance.ImageProvider
 import androidx.glance.LocalSize
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.ImageProvider
 import androidx.glance.appwidget.SizeMode
@@ -105,6 +106,7 @@ class StockAppWidget : GlanceAppWidget() {
     @Composable
     private fun Small(stateCount: Float) {
         Column(modifier = GlanceModifier
+            .clickable { refreshPrice() }
             .fillMaxSize()
             .background(GlanceTheme.colors.background)
             .padding(8.dp)) {
@@ -116,6 +118,7 @@ class StockAppWidget : GlanceAppWidget() {
     private fun Medium(stateCount: Float) {
         Column(horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
             modifier = GlanceModifier
+                .clickable { refreshPrice() }
                 .fillMaxSize()
                 .cornerRadius(15.dp)
                 .background(GlanceTheme.colors.background)
@@ -130,6 +133,9 @@ class StockAppWidget : GlanceAppWidget() {
                     .padding(20.dp)
             )
         }
+    }
+    private fun refreshPrice() {
+        PriceDataRepo.update()
     }
 
 }
